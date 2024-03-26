3627c1d4bf09e043ba7378fc4bd949e22abfb6d1
刷卡自信一点 <shuakabushi0@qq.com>
2024-03-26 21:15:15 +0800

    尝试修复部署失败（4）

--- apps/www/app/content/mail/page.tsx
+++ apps/www/app/content/mail/page.tsx
@@ -10,6 +10,12 @@
  */
 
 
+/*
+ * Copyright (C) 2023-2024 ByteFreezeLab×Sdjz.Wiki. All rights reserved.
+ * This project is strictly confidential and proprietary to the owner. It is not open-sourced and is not available for public use, distribution, or modification in any form. Unauthorized use, distribution, reproduction, or any other form of exploitation is strictly prohibited.
+ */
+
+
 // @ts-ignore
 import Cookies from 'js-cookie';
 import axios from 'axios';
