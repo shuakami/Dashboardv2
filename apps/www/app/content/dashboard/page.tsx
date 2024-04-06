0dff3f9b05dd995ed8ff3b1baa4c8ed4ba1380c3
刷卡自信一点 <shuakabushi0@qq.com>
2024-04-06 21:00:31 +0800

    （修正）修复部署报错

--- apps/www/app/content/dashboard/page.tsx
+++ apps/www/app/content/dashboard/page.tsx
@@ -2,15 +2,8 @@
  * Copyright (C) 2023-2024 ByteFreezeLab×Sdjz.Wiki. All rights reserved.
  * This project is strictly confidential and proprietary to the owner. It is not open-sourced and is not available for public use, distribution, or modification in any form. Unauthorized use, distribution, reproduction, or any other form of exploitation is strictly prohibited.
  */
-
+"use client"
 import { Button } from "@/registry/new-york/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/registry/new-york/ui/card"
 import {
   Tabs,
   TabsContent,
